b732092e78865cca484969d9572167e271b3b899
Eric Syme <spotlightphotoseattle@gmail.com>
2019-05-18 11:24:37 -0700

    portfolio clean commit

---
+++ portfolio.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $("#contactModalBtn").click(function(){
+      $("#contactModal").modal();
+    });
+});
+
+$(document).ready(function(){
+    $("#navCertBtn").click(function(){
+      $("#certificates").modal();
+    });
+});
